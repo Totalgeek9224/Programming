fa0f2764eb62f591c2a80c890bceb1285043a42c
Zach <46223001+Totalgeek9224@users.noreply.github.com>
2020-02-26 12:01:29 +0000

    Update Coursework.py

--- Coursework.py
+++ Coursework.py
@@ -1,3 +1,53 @@
+
+import random
+##############################################################
+##################### Declare Statements #####################
+##############################################################
+userInitials = []
+gameBoard = [ [ "E", "E", "E" ], [ "E", "E", "E" ],[ "E", "E", "E" ] ]
+firstMove = []
+#####################################################
+##################### Functions #####################
+#####################################################
+
+# Function to collect user 3 Initials, preventing numbers, and presenting them capitalised. Also recognises "quit" input, forcing the program to quit
+def collectInitials ():
+
+    # Ask user for Initials
+    userInitials = input('Enter your 3 initials: ').upper()
+    if userInitials == ("quit" or "Quit"):
+        quit()
+
+    # Confirm only letters
+    while (userInitials.isalpha() == False) or not (len(userInitials) == 3):
+        print('Incorrect Formatting- Please try again')
+        userInitials = input('Enter your 3 initials: ').upper()
+        if userInitials == ("quit" or "Quit"):
+            quit()
+
+    print ("Your initials are:" + userInitials)
+
+def drawBoard ():
+    print ("  A B C")
+    print ("1 " + gameBoard[1] + "|" + gameBoard[2] + "|" + gameBoard[3])
+    print ("2 _|_|_")
+    print ("3 _|_|_")
+
+
+def gameStart ():
+    firstPlay = random.choice(["Player", "Computer"])
+    print("The first move goes to: " + firstPlay )
+
+    if firstPlay == "Player":
+        print ("You are O's ")
+        firstMove = input ("Where would you like your first move?: ")
+
+    else:
+        print ("You are X's, your move is next.")
+
+#####################################################
+##################### Main Code #####################
+#####################################################
 print('''\  
   _______ __      ______              ______         
  /_  __(_) /__   /_  __/___ ______   /_  __/___  ___ 
@@ -8,40 +58,7 @@ print('''\
                   By Zachary DeMarco                 
                                                 
                                                              ''')
-#
-##########################
-### Declare Statements ###
-##########################
-
-userInitials = []
-
-
-################
-### Functions ###
-################
-
-# Funtion to collect user Initials, preventing numbers, while also confirming user intitals before proceeding.
-# Additionally, 
-def collectInitials ():
-
-    # Ask user for Initials
-    userInitials = input('Enter your 3 initials: ')
-    if userInitials == ("quit" or "Quit"):
-        quit()
-
-    # Confirm only letters
-    while (userInitials.isalpha() == False) or not (len(userInitials) == 3):
-        print('Incorrect Formatting- Please try again')
-        userInitials = input('Enter your 3 initials: ')
-        if userInitials == ("quit" or "Quit"):
-            quit()
-
-    print ("Your initials are:" + userInitials.upper())
-
-    
-#################
-### Main Code ###
-#################
 
 collectInitials ()
-
+gameStart ()
+#drawBoard ()
